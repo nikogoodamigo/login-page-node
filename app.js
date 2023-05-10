6bb3194c8d94546c12de20fb87ae2cc4c8e3fa9a
Danny <dn.webdev@yahoo.com>
2023-05-10 11:17:31 +0200

    added safeguards in case of wrong login credentials

--- app.js
+++ app.js
@@ -2,27 +2,43 @@ const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const crypto = require('crypto');
+const bodyParser = require('body-parser');
+
 
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 
 const secret = crypto.randomBytes(64).toString('hex');
 
-app.get('/api/createToken', (req, res)=>{
+app.post('/api/createToken', (req, res) => {
+  const {username, password} = req.body;
 
-  const currentTime = new Date();
-  const expiresIn = 60 * 60;
-  const expiresAt = new Date(currentTime.getTime() + expiresIn * 1000);
+  if (username === 'username' && password === 'passwd') {
+    const currentTime = new Date();
+    const expiresIn = 60 * 60;
+    const expiresAt = new Date(currentTime.getTime() + expiresIn * 1000);
+    const responseMessage ='';
+    const responseCode = 0;
 
-  this.secret = secret;
-  const token = jwt.sign({id: 123, secret}, secret, {expiresIn});
-  res.send({
-    token, 
-    created_at: currentTime.toLocaleString(), 
-    expires_at: expiresAt.toLocaleString()
-  });
+    this.secret = secret;
+    const token = jwt.sign({id: 123, secret}, secret, {expiresIn});
+
+    res.send({
+
+      token,
+      created_at: currentTime.toLocaleString(),
+      expires_at: expiresAt.toLocaleString()
+    });
+  } else {
+    res.send({responseCode: 401, responseMessage: 'Invalid credentials'});
+  }
 });
+
+  
 
 app.listen(3001, () => {
   console.log('Server is running on port 3001');
